46e161b290acb4238d81c0146d8329f8f46c68bd
Kori Jasmine Okeshola <148779548+koriokeshola@users.noreply.github.com>
2025-10-12 21:45:12 +0100

    Delete gol.h

--- gol.h
+++
@@ -1,8 +0,0 @@
-// Macros
-#define CELL(I,J) (field[size*(I)+(J)])
-#define ALIVE(I,J) t[size*(I)+(J)] = 1
-#define DEAD(I,J)  t[size*(I)+(J)] = 0
-
-// Function prototypes
-int count_alive(const char *field, int i, int j, int size) { };
-void evolve(const char *field, char *t, int size) { };
